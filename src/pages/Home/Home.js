73282691627b4773df356eac98c1144f4b5ffdbf
Abdo <abdopoetry@gmail.com>
2018-12-13 12:04:43 +0200

    Configured the router

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Home.scss";
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+        <h1>Home</h1>
+      </>
+    );
+  }
+}
+
+export default Home;
